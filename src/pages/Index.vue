ccb348abfea31b5e12d6bd760627b7f30edf67cb
Chiheb Chebbi <chiheb-chebbi@outlook.fr>
2022-12-03 16:38:39 -0500

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -32,7 +32,7 @@ export default {
   },
   data() {
     return {
-      description: 'The ultimate static generated documentation theme for the JAM-stack'
+      description: 'WELCOME TO BLUE TEAMS ACADEMY'
     }
   },
   metaInfo() {
